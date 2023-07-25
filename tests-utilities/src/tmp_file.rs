6214a35c715f4c4902feec37f1a40628c4683859
Tom-theCoder <tom.swirgon@gmail.com>
2023-07-25 11:24:19 +0200

    tests-utilities new file with parent and name

--- tests-utilities/src/tmp_file.rs
+++ tests-utilities/src/tmp_file.rs
@@ -21,6 +21,12 @@ impl TmpFile {
         Self { path }
     }
 
+    pub fn new_with_parent_and_name(parent: &Path, name: &str) -> Self {
+        let path = parent.join(name);
+        File::create(&path).unwrap();
+        Self { path }
+    }
+
     pub fn path(&self) -> &Path {
         self.path.as_path()
     }
@@ -81,5 +87,51 @@ mod test {
 
         fs::remove_dir(&parent_path).unwrap();
         assert!(is_parent_correct);
+    }
+
+    #[test]
+    fn new_with_parent_and_name_file_exist() {
+        let parent_path = tmp_path();
+        fs::create_dir(&parent_path).unwrap();
+
+        let file_exits;
+        {
+            let child = TmpFile::new_with_parent_and_name(&parent_path, "valid name.txt");
+            file_exits = child.path().is_file();
+        }
+
+        fs::remove_dir(&parent_path).unwrap();
+        assert!(file_exits);
+    }
+
+    #[test]
+    fn new_with_parent_and_name_correct_parent() {
+        let parent_path = tmp_path();
+        fs::create_dir(&parent_path).unwrap();
+
+        let is_parent_correct;
+        {
+            let child = TmpFile::new_with_parent_and_name(&parent_path, "valid name.txt");
+            is_parent_correct = child.path().parent().unwrap() == parent_path;
+        }
+
+        fs::remove_dir(&parent_path).unwrap();
+        assert!(is_parent_correct);
+    }
+
+    #[test]
+    fn new_with_parent_and_name_correct_name() {
+        let parent_path = tmp_path();
+        fs::create_dir(&parent_path).unwrap();
+        let name = "filename.txt";
+
+        let is_name_correct;
+        {
+            let child = TmpFile::new_with_parent_and_name(&parent_path, name);
+            is_name_correct = child.path().file_name().unwrap().to_string_lossy() == name;
+        }
+
+        fs::remove_dir(&parent_path).unwrap();
+        assert!(is_name_correct);
     }
 }
